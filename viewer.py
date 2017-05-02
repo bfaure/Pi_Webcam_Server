515caa17997192a1f83543da600d9c247e639288
Brian Faure <bfaure23@gmail.com>
2017-05-02 18:36:29 -0400

    added ip connection window

--- viewer.py
+++ viewer.py
@@ -29,7 +29,6 @@ class frame_client(QThread):
 		client.download('')
 '''	
 
-
 class frame_manager(QThread): # handles updating the gui
 	update_gui = pyqtSignal()
 
@@ -64,10 +63,90 @@ class frame_manager(QThread): # handles updating the gui
 				num_transmission_errors+=1
 			time.sleep(self.refresh_after)
 
+class ip_window(QWidget):
+	got_ip = pyqtSignal()
+
+	def __init__(self,parent=None):
+		super(ip_window,self).__init__()
+		self.parent = parent
+		self.connect(self,SIGNAL("got_ip()"),self.parent.got_ip)
+		self.init_vars()
+		self.init_ui()
+
+	def init_vars(self):
+		self.full_ip = None 
+		self.ip1 = "192"
+		self.ip2 = "168"
+		self.ip3 = "1"
+		self.ip4 = ""
+
+	def init_ui(self):
+		self.setWindowTitle("IP Address Input")
+		self.layout = QVBoxLayout(self)
+		self.top_label = QLabel("Enter IP Address of other user:")
+		self.layout.addWidget(self.top_label)
+
+		ip_row = QHBoxLayout()
+
+		self.ip1_input = QLineEdit(self.ip1)
+		self.ip2_input = QLineEdit(self.ip2)
+		self.ip3_input = QLineEdit(self.ip3)
+		self.ip4_input = QLineEdit(self.ip4)
+
+		ip_row.addSpacing(15)
+		ip_row.addWidget(self.ip1_input)
+		ip_row.addWidget(QLabel("."))
+		ip_row.addWidget(self.ip2_input)
+		ip_row.addWidget(QLabel("."))
+		ip_row.addWidget(self.ip3_input)
+		ip_row.addWidget(QLabel("."))
+		ip_row.addWidget(self.ip4_input)
+		ip_row.addSpacing(15)
+
+		ok_cancel_row = QHBoxLayout()
+		ok_button = QPushButton("Ok")
+		cancel_button = QPushButton("Cancel")
+
+		ok_cancel_row.addStretch()
+		ok_cancel_row.addWidget(cancel_button)
+		ok_cancel_row.addWidget(ok_button)
+		ok_cancel_row.addStretch()
+
+		ok_button.clicked.connect(self.ok_selected)
+		cancel_button.clicked.connect(self.cancel_selected)
+
+		self.layout.addLayout(ip_row)
+		self.layout.addLayout(ok_cancel_row)
+		self.layout.addSpacing(20)
+
+		self.setFixedWidth(350)
+		self.setFixedHeight(150)
+
+	def keyPressEvent(self,e):
+		if e.key() in [Qt.Key_Return,Qt.Key_Enter]:
+			self.ok_selected()
+
+	def ok_selected(self):
+		self.ip1 = str(self.ip1_input.text())
+		self.ip2 = str(self.ip2_input.text())
+		self.ip3 = str(self.ip3_input.text())
+		self.ip4 = str(self.ip4_input.text())
+		self.full_ip = self.ip1+"."+self.ip2+"."+self.ip3+"."+self.ip4
+		self.got_ip.emit()
+
+	def cancel_selected(self):
+		self.full_ip = None
+		self.got_ip.emit()
+
+	def closeEvent(self,e):
+		self.cancel_selected()
+
+
 class main_window(QWidget): 
 
 	def __init__(self):
 		super(main_window,self).__init__() 
+		self.ip_dialog_window = ip_window(self)
 		self.init_vars()
 		self.init_ui()
 
@@ -107,9 +186,18 @@ class main_window(QWidget):
 		self.current_frame = QPixmap(self.current_frame_file)
 		self.main_image.setPixmap(self.current_frame)
 
-	def connect_to_server(self):
-		print("Connecting to server...")
-		self.fps_manager.ip_address="localhost"
+	def got_ip(self):
+		self.ip_dialog_window.hide()
+		self.show()
+		self.connect_to_server(ip=self.ip_dialog_window.full_ip)
+
+	def connect_to_server(self,ip=None):
+		if ip==None:
+			self.hide()
+			self.ip_dialog_window.show()
+			return
+		print("Connecting to %s"%ip)
+		self.fps_manager.ip_address=ip
 		self.fps_manager.port_num=DEFAULT_PORT_NUM
 		self.fps_manager.pause=False 
 
